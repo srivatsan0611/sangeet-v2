1cfacb3651f08ab9abc348090a0dea5a361b39c1
Srivatsan Srinivasan <srivatsansrinivasan03@gmail.com>
2025-12-22 20:56:06 +0530

    fix: add try-catch & fix es6 import modules for __dirname

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ import { createServer } from 'https';
 import { parse } from 'url';
 import next from 'next';
 import { readFileSync } from 'fs';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: readFileSync(join(__dirname, '.certificates', 'localhost-key.pem')),
-  cert: readFileSync(join(__dirname, '.certificates', 'localhost.pem')),
-};
+
+let httpsOptions;
+
+try {
+  httpsOptions = {
+    key: readFileSync(join(__dirname, '.certificates', 'localhost-key.pem')),
+    cert: readFileSync(join(__dirname, '.certificates', 'localhost.pem')),
+  };
+} catch (err) {
+  console.error(
+    'Failed to load HTTPS Certificates from ".certificates' + '\nPlease ensure to follow docs/HTTPS_SETUP.md' 
+    + `\nError thrown: ${err}`
+  )
+  process.exit(1);
+}
+
 
 const port = process.env.PORT || 3000
 
