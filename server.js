2a745069e26c243b2d67598542024b8da0e0f9ea
Srivatsan Srinivasan <srivatsansrinivasan03@gmail.com>
2025-12-22 20:38:17 +0530

    add: custom server.js entry point to make server run in https

---
+++ server.js
@@ -0,0 +1,24 @@
+import { createServer } from 'https';
+import { parse } from 'url';
+import next from 'next';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: readFileSync(join(__dirname, '.certificates', 'localhost-key.pem')),
+  cert: readFileSync(join(__dirname, '.certificates', 'localhost.pem')),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:3000');
+  });
+});
